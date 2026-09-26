41926fe11436250c9e947c3b3b712e2f2a880f13
nur12islam <68411946+nur12islam@users.noreply.github.com>
2026-09-26 23:52:57 +0530

    feat: register plugin manager command

--- src/bot/pluginManager.ts
+++ src/bot/pluginManager.ts
@@ -125,6 +125,7 @@ export async function loadPlugins(client: TelegramClient) {
     await import("./plugins/cookies.js"),
     await import("./plugins/help.js"),
     await import("./plugins/menu.js"),
+    await import("./plugins/plugin.js"),
     await import("./plugins/eval.js"),
     await import("./plugins/alive.js"),
     await import("./plugins/ci.js"),
